2105ebe97b2b4654f0b814396101208605f6c4b9
refanda <dev.refanda@gmail.com>
2024-03-25 10:15:51 +0700

    Add containerization

--- mygram/main.go
+++ mygram/main.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+		log.Printf("Error loading .env file: %v", err)
 	}
 
 	postgres := database.NewPostgres()
